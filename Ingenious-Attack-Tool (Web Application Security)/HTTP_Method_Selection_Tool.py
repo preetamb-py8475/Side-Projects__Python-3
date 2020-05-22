a1de02f7930f7cc1a0bfe39d4099235c73f6ccdb
Preetam Baruah <47924746+preetamb-py8475@users.noreply.github.com>
2020-05-23 01:30:29 +0530

    Add files via upload

---
+++ Ingenious-Attack-Tool (Web Application Security)/HTTP_Method_Selection_Tool.py
@@ -0,0 +1,33 @@
+
+import requests
+
+
+def select_http_method(m):
+	"""
+                    My Python Script : HTTP Method Selection Tool
+                    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+	"""
+
+	m = m.lower()
+    print(f"HTTP Method : {m}")
+
+    if m == 'get':
+        return requests.get
+
+    elif m == 'post':
+        return requests.post
+
+    elif m == 'put':
+        return requests.put
+
+    elif m == 'delete':
+        return requests.delete
+
+    elif m == 'head':
+        return requests.head
+
+    elif m == 'options':
+        return requests.options
+
+    elif m == 'patch':
+        return requests.patch
